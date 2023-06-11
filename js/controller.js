aa7e7764bda2cce6ac9ebfda218e06ad5ea3133f
Michota <dawidmichota@gmail.com>
2023-06-11 12:38:18 +0200

    createProject() creates new project with unique ID.

--- js/controller.js
+++ js/controller.js
@@ -2,20 +2,6 @@ import * as model from "./model.js";
 // import * as view from "./views/view.js";
 import newProject from "./views/newProject.js";
 
-const controlNewProject = async function () {
-  try {
-    const projectName = prompt("Name new project");
-    console.log(projectName);
-    if (projectName === "") throw "nie dziala";
-    const markup = newProject.createHTML(projectName);
-    newProject.renderHTML(markup);
-  } catch (error) {
-    newProject.renderError(error);
-  }
-};
-
-console.log("test");
-
 const init = function () {
   // controlNewProject();
 };
---
+++ js/model.js
@@ -0,0 +1,54 @@
+export const state = {
+  project: {
+    id: 0,
+    projectName: "",
+    icon: "",
+    tasks: [],
+  },
+  projectsArr: [],
+};
+
+// Create project object
+export const createProject = function (nameInput, iconInput = "terminal") {
+  const { project } = state;
+  return {
+    id: createId(),
+    // TODO: change 'terminal' to icon selected by user
+    icon: iconInput,
+    projectName: nameInput,
+    tasks: [],
+  };
+};
+
+// Create new ID
+const createId = function (arrToCheck = state.projectsArr) {
+  // generate 4 digits number, that does not starts with "0"
+  const generateNumber = function () {
+    let generatedNumber;
+    for (let i = 0; i < 1; i = i) {
+      let num = Math.random().toString().slice(-4);
+      if (num[0] !== "0" && num.length === 4) {
+        i++;
+        generatedNumber = Number(num);
+      } else {
+        i = 0;
+      }
+    }
+    return generatedNumber;
+  };
+  const newId = generateNumber();
+
+  // Check if ID is not already used by another object
+  if (checkIdUnique(arrToCheck, newId)) {
+    return newId;
+  } else {
+    return createId();
+  }
+};
+
+const checkIdUnique = function (arr, id) {
+  // take IDs out of array
+  const iDs = arr.map((el) => el.id);
+  return iDs.find((el) => el === id) ? false : true;
+  // if ID is unique, then return TRUE
+};
